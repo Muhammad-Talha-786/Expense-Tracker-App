90c664e5d5df1c385a405f21a773c5ed82cfa74b
Muhammad Talha <talhaofficialwork@gmail.com>
2021-10-23 04:31:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { TransactionList } from "./components/TransactionList";
-
 import { GlobalProvider } from "./context/GlobalState";
-
-import './App.css';
 
 function App() {
   return (
